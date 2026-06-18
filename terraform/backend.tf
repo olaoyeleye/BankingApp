6d434ed4a338d9ae980bdc756ed5a1f166098ac9
Olu <olaoyeleye@yahoo.co.uk>
2026-06-18 22:27:06 +0100

    setup banking application backend and exclude terraform cache

---
+++ terraform/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket  = "techbleat-bank-app"
+    key     = "bank-app-statefile/terraform.tfstate"
+    region  = "eu-west-1"
+    encrypt = true
+  }
+}
---
+++ terraform/variable.tf
@@ -0,0 +1,8 @@
+variable "region" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "key_name" {
+    default = "kunle-vpc-deployer-key"
+}
+variable "instance-name-nginx" {} 
+variable "vpc_name" {}
