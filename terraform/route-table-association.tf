cfb5d12fbed97842fc0efbbfbc4700a51681e11f
Olu <olaoyeleye@yahoo.co.uk>
2026-06-19 02:03:56 +0100

    rout table association

--- terraform/route-table-association.tf
+++ terraform/route-table-association.tf
@@ -1,7 +1,14 @@
-resource "aws_route_table_association" "public-association" {
+resource "aws_route_table_association" "public_1" {
   subnet_id      = aws_subnet.public-kunle-subnet.id
   route_table_id = aws_route_table.public-kunle-rt.id
 }
+
+resource "aws_route_table_association" "public_2" {
+  subnet_id      = aws_subnet.public-kunle-subnet-2.id
+  route_table_id = aws_route_table.public-kunle-rt.id
+}
+
+
 
 
 resource "aws_route_table_association" "private-association" {
