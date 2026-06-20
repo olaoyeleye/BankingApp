c1ab24cc5e69b40310b2409345ec8dfbad33b1b2
Olu <olaoyeleye@yahoo.co.uk>
2026-06-20 02:46:09 +0100

    added frontend

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -163,3 +163,9 @@ resource "aws_eks_addon" "ebs_csi" {
     aws_iam_role_policy_attachment.ebs_csi_policy
   ]
 }
+
+
+resource "aws_iam_role_policy_attachment" "eks_nodes_ebs_csi" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.eks_nodes.name
+}
