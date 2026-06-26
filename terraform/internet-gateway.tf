5223c0b5d26f6a9c6da1e84f53c16a07ea6178f5
Olu <olaoyeleye@yahoo.co.uk>
2026-06-26 19:09:20 +0100

    apply

--- terraform/internet-gateway.tf
+++ terraform/internet-gateway.tf
@@ -1,8 +1,6 @@
 resource "aws_internet_gateway" "kunle-igw" {
   vpc_id = aws_vpc.vpc.id
-  depends_on = [
-    aws_eks_node_group.main
-  ]
+  #depends_on = [    aws_eks_node_group.main  ]
   tags = {
     Name = "kunle-igw"
   }
