915af98a70138d948a97af5c0df40ef25f4008db
Olu <olaoyeleye@yahoo.co.uk>
2026-07-02 15:23:42 +0100

    dev changed for ingres endpoint

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.28.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.12"
@@ -15,6 +19,17 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+provider "kubernetes" {
+  host                   = aws_eks_cluster.main.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name, "--region", "eu-west-1"]
+  }
+}
+
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.main.endpoint
@@ -22,8 +37,8 @@ provider "helm" {
 
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
       command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name, "--region", "eu-west-1"]
     }
   }
 }
