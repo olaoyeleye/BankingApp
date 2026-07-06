fea364ea67f611042e8c5dd13bc5337704c8b3e9
Olu <olaoyeleye@yahoo.co.uk>
2026-07-06 13:57:43 +0100

    new

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -7,7 +7,7 @@ variable "key_name" {
 variable "instance-name-nginx" {} 
 variable "vpc_name" {}
 
-variable "eks_admin_principal_arn" {
-  description = "IAM principal ARN that should have EKS cluster admin access"
+variable "eks_admin_principal_arn" { 
   type        = string
 }
+
