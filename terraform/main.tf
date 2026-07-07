544ab8a497a77e6fa7f2a91cae61068693a134d6
Olu <olaoyeleye@yahoo.co.uk>
2026-07-08 00:07:08 +0100

    apply

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,45 +4,39 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.28.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.32"
+    }
     helm = {
       source  = "hashicorp/helm"
       version = "~> 2.12"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.25"
     }
   }
 }
 
 provider "aws" {
-  region = var.region
+  region = "eu-west-1"
 }
 
 data "aws_eks_cluster" "main" {
   name = aws_eks_cluster.main.name
 }
 
+data "aws_eks_cluster_auth" "main" {
+  name = aws_eks_cluster.main.name
+}
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.main.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-
-  exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    command     = "aws"
-    args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
-  }
+  token                  = data.aws_eks_cluster_auth.main.token
 }
 
 provider "helm" {
   kubernetes {
     host                   = data.aws_eks_cluster.main.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      command     = "aws"
-      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
-    }
+    token                  = data.aws_eks_cluster_auth.main.token
   }
 }
