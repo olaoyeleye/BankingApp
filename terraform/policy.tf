480a4a2b9416900fba4965848cc2df1c179d04a1
Olu <olaoyeleye@yahoo.co.uk>
2026-07-08 00:31:28 +0100

    apply

--- terraform/policy.tf
+++ terraform/policy.tf
@@ -1,10 +1,52 @@
-# EKS cluster role policy
+resource "aws_iam_role" "eks_cluster" {
+  name = "${var.vpc_name}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.vpc_name}-eks-cluster-role"
+  }
+}
+
+resource "aws_iam_role" "eks_nodes" {
+  name = "${var.vpc_name}-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.vpc_name}-eks-node-role"
+  }
+}
+
+
+
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_cluster.name
 }
 
-# EKS node role policies
 resource "aws_iam_role_policy_attachment" "eks_nodes_worker_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.eks_nodes.name
--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -1,6 +1,6 @@
-resource "aws_security_group" "public_kunle_sg" {
+resource "aws_security_group" "public-kunle-sg" {
   name        = "public-kunle-sg"
-  description = "Public security group"
+  description = "Allow public inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc.vpc.id
 
   tags = {
@@ -8,69 +8,69 @@ resource "aws_security_group" "public_kunle_sg" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_https" {
-  security_group_id = aws_security_group.public_kunle_sg.id
+resource "aws_vpc_security_group_ingress_rule" "allow_HTTPS" {
+  security_group_id = aws_security_group.public-kunle-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
+  ip_protocol       = "tcp"
   to_port           = 443
-  ip_protocol       = "tcp"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-  security_group_id = aws_security_group.public_kunle_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  to_port           = 80
-  ip_protocol       = "tcp"
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.public_kunle_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  to_port           = 22
-  ip_protocol       = "tcp"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_user" {
-  security_group_id = aws_security_group.public_kunle_sg.id
+  security_group_id = aws_security_group.public-kunle-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 8000
-  to_port           = 8000
   ip_protocol       = "tcp"
+  to_port           = 8000
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_activities" {
-  security_group_id = aws_security_group.public_kunle_sg.id
+resource "aws_vpc_security_group_ingress_rule" "allow-activities" {
+  security_group_id = aws_security_group.public-kunle-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 8001
-  to_port           = 8001
   ip_protocol       = "tcp"
+  to_port           = 8001
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_transaction" {
-  security_group_id = aws_security_group.public_kunle_sg.id
+  security_group_id = aws_security_group.public-kunle-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 8080
-  to_port           = 8080
   ip_protocol       = "tcp"
+  to_port           = 8080
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_postgres_public" {
-  security_group_id = aws_security_group.public_kunle_sg.id
+resource "aws_vpc_security_group_ingress_rule" "allow_postgress" {
+  security_group_id = aws_security_group.public-kunle-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 5432
-  to_port           = 5432
   ip_protocol       = "tcp"
+  to_port           = 5432
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_public_traffic" {
-  security_group_id = aws_security_group.public_kunle_sg.id
+resource "aws_vpc_security_group_ingress_rule" "allow_HTTP" {
+  security_group_id = aws_security_group.public-kunle-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_SSH" {
+  security_group_id = aws_security_group.public-kunle-sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow-all-public-traffic" {
+  security_group_id = aws_security_group.public-kunle-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
 
-resource "aws_security_group" "private_kunle_sg" {
+resource "aws_security_group" "private-kunle-sg" {
   name   = "private-kunle-sg"
   vpc_id = aws_vpc.vpc.id
 
@@ -79,24 +79,24 @@ resource "aws_security_group" "private_kunle_sg" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_postgresql_private" {
-  security_group_id = aws_security_group.private_kunle_sg.id
+resource "aws_vpc_security_group_ingress_rule" "allow-postgreSQL" {
+  security_group_id = aws_security_group.private-kunle-sg.id
   cidr_ipv4         = aws_vpc.vpc.cidr_block
   from_port         = 5432
-  to_port           = 5432
   ip_protocol       = "tcp"
+  to_port           = 5432
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_private_ssh" {
-  security_group_id = aws_security_group.private_kunle_sg.id
+resource "aws_vpc_security_group_ingress_rule" "allow-private-SSH" {
+  security_group_id = aws_security_group.private-kunle-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 22
-  to_port           = 22
   ip_protocol       = "tcp"
+  to_port           = 22
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_private_traffic" {
-  security_group_id = aws_security_group.private_kunle_sg.id
+resource "aws_vpc_security_group_egress_rule" "allow-all-private-traffic" {
+  security_group_id = aws_security_group.private-kunle-sg.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
@@ -107,7 +107,7 @@ resource "aws_security_group_rule" "allow_ec2_to_eks_api" {
   to_port                  = 443
   protocol                 = "tcp"
   security_group_id        = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
-  source_security_group_id = aws_security_group.public_kunle_sg.id
+  source_security_group_id = aws_security_group.public-kunle-sg.id
   description              = "Allow EC2/Ansible to reach EKS API"
 }
 
@@ -116,7 +116,7 @@ resource "aws_security_group_rule" "allow_cluster_to_nodes" {
   from_port                = 1025
   to_port                  = 65535
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.public_kunle_sg.id
+  security_group_id        = aws_security_group.public-kunle-sg.id
   source_security_group_id = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
   description              = "Allow EKS cluster control plane to communicate with nodes"
 }
@@ -126,13 +126,13 @@ resource "aws_security_group_rule" "allow_nodes_to_cluster" {
   from_port                = 443
   to_port                  = 443
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.public_kunle_sg.id
+  security_group_id        = aws_security_group.public-kunle-sg.id
   source_security_group_id = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
   description              = "Allow nodes to communicate with EKS cluster control plane"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_node_to_node" {
-  security_group_id = aws_security_group.public_kunle_sg.id
+  security_group_id = aws_security_group.public-kunle-sg.id
   cidr_ipv4         = aws_vpc.vpc.cidr_block
   ip_protocol       = "-1"
   description       = "Allow nodes to communicate with each other"
