4c971cd3e07205ec7f9e8386ef5e7083f900b40c
Olu <olaoyeleye@yahoo.co.uk>
2026-07-07 23:52:56 +0100

    destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.28.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
 data "aws_eks_cluster" "main" {
   name = aws_eks_cluster.main.name
 }
@@ -14,10 +35,11 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes = {
+  kubernetes {
     host                   = data.aws_eks_cluster.main.endpoint
     cluster_ca_certificate = base64decode(data.aws_eks_cluster.main.certificate_authority[0].data)
-    exec = {
+
+    exec {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.main.name]
