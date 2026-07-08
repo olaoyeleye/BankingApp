2da37e57b47ee2ecf3d0dae3fe298cac15113fc5
Olu <olaoyeleye@yahoo.co.uk>
2026-07-09 00:23:00 +0100

    apply

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,22 @@ terraform {
       source  = "hashicorp/aws"
       version = "6.28.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.12.0" # Keeps your Helm provider modern and compatible
+    }
+  }
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.main.name
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.main.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.main.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
