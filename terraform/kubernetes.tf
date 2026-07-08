f8eedee5500b326fbe9ccefa0fae7219555cca66
Olu <olaoyeleye@yahoo.co.uk>
2026-07-08 22:52:02 +0100

    apply

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -68,3 +68,55 @@ resource "aws_eks_addon" "ebs_csi" {
     aws_iam_role_policy_attachment.eks_nodes_ebs_csi
   ]
 }
+
+# EBS CSI IAM Role
+resource "aws_iam_role" "ebs_csi_role" {
+  name = "${var.vpc_name}-ebs-csi-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Federated = aws_iam_openid_connect_provider.eks.arn
+      }
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Condition = {
+        StringEquals = {
+          "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:sub" = "system:serviceaccount:kube-system:ebs-csi-controller-sa"
+          "${replace(aws_iam_openid_connect_provider.eks.url, "https://", "")}:aud" = "sts.amazonaws.com"
+        }
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ebs_csi_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.ebs_csi_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_nodes_ebs_csi" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = data.aws_iam_role.eks_nodes.name
+}
+
+# OIDC Provider (needed by ebs_csi_role above)
+data "tls_certificate" "eks" {
+  url = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.main.identity[0].oidc[0].issuer
+}
+
+
+data "aws_iam_role" "eks_cluster" {
+  name = "${var.vpc_name}-eks-cluster-role"
+}
+
+data "aws_iam_role" "eks_nodes" {
+  name = "${var.vpc_name}-eks-node-role"
+}
