057c065fc55dad3924d4a419e04bfd36f366bc75
Olu <olaoyeleye@yahoo.co.uk>
2026-07-08 01:22:46 +0100

    apply

---
+++ terraform/kubernetes.tf
@@ -0,0 +1,59 @@
+resource "aws_eks_cluster" "main" {
+  name     = "${var.vpc_name}-cluster"
+  role_arn = data.aws_iam_role.eks_cluster.arn
+  version  = "1.31"
+
+  vpc_config {
+    subnet_ids = [
+      aws_subnet.public-subnet-1.id,
+      aws_subnet.public-subnet-2.id,
+      aws_subnet.private-subnet-1.id,
+      aws_subnet.private-subnet-2.id
+    ]
+  }
+
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_cluster_policy
+  ]
+
+  tags = {
+    Name = "${var.vpc_name}-cluster"
+  }
+}
+
+resource "aws_eks_node_group" "main" {
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${var.vpc_name}-node-group"
+  node_role_arn   = data.aws_iam_role.eks_nodes.arn
+
+  subnet_ids = [
+    aws_subnet.private-subnet-1.id,
+    aws_subnet.private-subnet-2.id
+  ]
+
+  scaling_config {
+    desired_size = 2
+    min_size     = 1
+    max_size     = 3
+  }
+
+  instance_types = [var.instance_type]
+  ami_type       = "AL2_x86_64"
+  capacity_type  = "ON_DEMAND"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks_nodes_worker_policy,
+    aws_iam_role_policy_attachment.eks_nodes_cni_policy,
+    aws_iam_role_policy_attachment.eks_nodes_ecr_policy,
+    aws_eks_cluster.main
+  ]
+
+  tags = {
+    Name = "${var.vpc_name}-node-group"
+  }
+}
