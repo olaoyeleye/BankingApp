41128dca1ac090cc301f534bae24dd5c46a430dd
Olu <olaoyeleye@yahoo.co.uk>
2026-07-09 23:06:00 +0100

    apply

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -4,17 +4,42 @@ data "aws_availability_zones" "available" {
 
 data "aws_caller_identity" "current" {}
 
-data "aws_iam_role" "eks_cluster" {
+
+resource "aws_iam_role" "eks_cluster" {
   name = "${var.vpc_name}-eks-cluster-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
+
+
 
 resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = data.aws_iam_role.eks_cluster.name
 }
 
-data "aws_iam_role" "eks_nodes" {
+resource "aws_iam_role" "eks_nodes" {
   name = "${var.vpc_name}-eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
 resource "aws_iam_role_policy_attachment" "eks_worker_node_policy" {
