91906f2b7b8fef7fcc2771c7894a260995b704e0
Olu <olaoyeleye@yahoo.co.uk>
2026-07-09 16:33:49 +0100

    working

--- terraform/output.tf
+++ terraform/output.tf
@@ -8,3 +8,8 @@ output "eks_cluster_endpoint" {
   value       = aws_eks_cluster.main.endpoint
   description = "EKS cluster endpoint"
 }
+
+output "nginx_public_ip" {
+  value       = aws_instance.nginx.public_ip
+  description = "Public IP of Nginx server"
+}
