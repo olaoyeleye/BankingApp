e1d55f7d59553ff1f1ba83e8a22723cf794dd717
Olu <olaoyeleye@yahoo.co.uk>
2026-07-09 22:52:38 +0100

    apply

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "techbleat-bank-app"
-    key     = "bank-app-statefile/terraform.tfstate"
+    bucket  = "techbleats-bank-app"
+    key     = "uba-bank-app-statefile/terraform.tfstate"
     region  = "eu-west-1"
     encrypt = true
   }
