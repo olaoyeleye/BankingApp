975a4347a111707d08862fee2db0c337c40b9b6e
Olu <olaoyeleye@yahoo.co.uk>
2026-07-09 23:44:14 +0100

    apply

--- terraform/nginx_controller.tf
+++ terraform/nginx_controller.tf
@@ -1,19 +1,40 @@
+#resource "helm_release" "ingress_nginx" {
+#  name             = "ingress-nginx"
+#  repository       = "https://kubernetes.github.io/ingress-nginx"
+#  chart            = "ingress-nginx"
+#  namespace        = "ingress-nginx"
+#  create_namespace = true
+
+#  timeout          = 900
+#  wait             = true
+#  cleanup_on_fail  = true
+#  atomic           = false
+
+#  values = [yamlencode({
+#    controller = {
+#      service = {
+#        type = "LoadBalancer"
+#      }
+#    }
+#  })]
+
+#  depends_on = [
+#    aws_eks_node_group.main
+#  ]
+#}
+
 resource "helm_release" "ingress_nginx" {
   name             = "ingress-nginx"
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
   namespace        = "ingress-nginx"
   create_namespace = true
-
   timeout          = 900
-  wait             = true
-  cleanup_on_fail  = true
-  atomic           = false
 
   values = [yamlencode({
     controller = {
       service = {
-        type = "LoadBalancer"
+        type = "NodePort"
       }
     }
   })]
