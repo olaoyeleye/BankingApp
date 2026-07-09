0d289bb3ff8751a347330979849f21b3f21dd0e6
Olu <olaoyeleye@yahoo.co.uk>
2026-07-09 19:56:35 +0100

    working

--- terraform/nginx_controller.tf
+++ terraform/nginx_controller.tf
@@ -1,11 +1,3 @@
-data "aws_eks_cluster" "main" {
-  name = aws_eks_cluster.main.name
-}
-
-data "aws_eks_cluster_auth" "main" {
-  name = aws_eks_cluster.main.name
-}
-
 resource "helm_release" "ingress_nginx" {
   name             = "ingress-nginx"
   repository       = "https://kubernetes.github.io/ingress-nginx"
