22e41048bc4287492d7d1e35d3c74edd24508fc4
Olu <olaoyeleye@yahoo.co.uk>
2026-07-09 19:46:11 +0100

    working

---
+++ terraform/kubernetes_addons.tf
@@ -0,0 +1,13 @@
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name             = aws_eks_cluster.main.name
+  addon_name               = "aws-ebs-csi-driver"
+  service_account_role_arn = aws_iam_role.ebs_csi_role.arn
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+
+  depends_on = [
+    aws_iam_role_policy_attachment.ebs_csi_policy,
+    aws_eks_node_group.main
+  ]
+}
