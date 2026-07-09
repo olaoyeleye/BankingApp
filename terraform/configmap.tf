84828101d92878bee6b160c9d459c43d89ccf2b0
Olu <olaoyeleye@yahoo.co.uk>
2026-07-09 04:17:31 +0100

    destroy

--- terraform/configmap.tf
+++ terraform/configmap.tf
@@ -1,17 +1,1 @@
-data "aws_s3_object" "banking_config" {
-  bucket = "techbleat-bank-app"
-  key    = "terraform_manifest/configmap.json"
-}
-
-locals {
-  banking_config_data = jsondecode(data.aws_s3_object.banking_config.body)
-}
-
-resource "kubernetes_config_map_v1" "banking_config" {
-  metadata {
-    name      = "banking-config"
-    namespace = "banking"
-  }
-
-  data = local.banking_config_data
-}
+# Kubernetes ConfigMap resources are managed by Ansible, not Terraform.
--- terraform/secret.tf
+++ terraform/secret.tf
@@ -1,18 +1,1 @@
-data "aws_s3_object" "banking_secrets" {
-  bucket = "techbleat-bank-app"
-  key    = "terraform_manifest/secret.json"
-}
-
-locals {
-  banking_secret_data = jsondecode(data.aws_s3_object.banking_secrets.body)
-}
-
-resource "kubernetes_secret_v1" "banking_secrets" {
-  metadata {
-    name      = "banking-secrets"
-    namespace = "banking"
-  }
-
-  type = "Opaque"
-  data = local.banking_secret_data
-}
+# Kubernetes Secret resources are managed by Ansible, not Terraform.
