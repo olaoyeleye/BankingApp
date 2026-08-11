b4b5b1e504ce288d01246beb3e43f0c631bb3a0a
Olu <olaoyeleye@yahoo.co.uk>
2026-08-11 21:39:38 +0100

    apply dev

--- terraform/kubernetes_addons.tf
+++ terraform/kubernetes_addons.tf
@@ -1,15 +1,10 @@
-data "aws_eks_addon_version" "ebs_csi" {
-  addon_name = "aws-ebs-csi-driver"
-  # Use the most recent version marked as default by EKS for this cluster's Kubernetes version
-  most_recent = true
-}
-
 resource "aws_eks_addon" "ebs_csi" {
   cluster_name             = aws_eks_cluster.main.name
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = data.aws_eks_addon_version.ebs_csi.version
   service_account_role_arn = aws_iam_role.ebs_csi_role.arn
 
+  # Do not pin addon_version: EKS uses its default on create, and on import
+  # the deployed version is adopted. This avoids version-drift reconciliation loops.
   preserve                    = true
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "PRESERVE"
