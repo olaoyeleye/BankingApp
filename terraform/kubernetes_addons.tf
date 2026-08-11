924838d44c515e453c3e171b557880ae7b246ada
Olu <olaoyeleye@yahoo.co.uk>
2026-08-11 21:26:50 +0100

    apply dev

--- terraform/kubernetes_addons.tf
+++ terraform/kubernetes_addons.tf
@@ -1,10 +1,24 @@
+data "aws_eks_addon_version" "ebs_csi" {
+  addon_name = "aws-ebs-csi-driver"
+  # Use the most recent version marked as default by EKS for this cluster's Kubernetes version
+  most_recent = true
+}
+
 resource "aws_eks_addon" "ebs_csi" {
   cluster_name             = aws_eks_cluster.main.name
   addon_name               = "aws-ebs-csi-driver"
+  addon_version            = data.aws_eks_addon_version.ebs_csi.version
   service_account_role_arn = aws_iam_role.ebs_csi_role.arn
 
+  preserve                    = true
   resolve_conflicts_on_create = "OVERWRITE"
-  resolve_conflicts_on_update = "OVERWRITE"
+  resolve_conflicts_on_update = "PRESERVE"
+
+  timeouts {
+    create = "20m"
+    update = "20m"
+    delete = "20m"
+  }
 
   depends_on = [
     aws_iam_role_policy_attachment.ebs_csi_policy,
