62daf49f8bee6fd2fb3962b9579c6309bc469928
Olu <olaoyeleye@yahoo.co.uk>
2026-08-13 01:46:58 +0100

    apply dev

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -144,9 +144,9 @@ resource "aws_eks_node_group" "main" {
   ]
 
   scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
+    desired_size = 4
+    max_size     = 4
+    min_size     = 4
   }
 
   tags = {}
